ded0cc3b85aa61a7bb66f087eb20d605614478de
mingi-R6 <73065676+mingi-R6@users.noreply.github.com>
2026-03-23 11:37:34 +0900

    Add files via upload

---
+++ code/3장/3_data_type.py
@@ -0,0 +1,14 @@
+# 3_data_type.py
+a = 10
+b = 3.14
+c = "hello"
+d = 'goodbye'
+e = True
+
+print(f"{a}: {type(a)}") # f: format (서식문자열 {변수}") 
+print(f"{b}: {type(b)}")
+print(f"{c}: {type(c)}")
+print(f"{d}: {type(d)}")
+print(f"{e}: {type(e)}")
+
+print(a, ":", type(a))
